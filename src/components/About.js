859362348674b12fca29592fb36eab35d5613d4f
Harsh Gaur <hgaur701@gmail.com>
2023-12-07 22:51:44 +0530

    cleaning code in About , Home

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,10 @@
 import React from 'react'
-import { useContext } from 'react'
-import { useEffect } from 'react';
-import NoteContext from '../context/notes/noteContext'
 
 export default function About() {
 
-  const noteVars = useContext(NoteContext);
-  useEffect(() => {
-    noteVars.update('About');
-  }, [])
-
   return (
     <div className='About'>
-      <div>This is {noteVars.state.name}</div>
+      <div>This is About</div>
     </div>
   )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,10 @@
 import React from 'react'
-import { useContext } from 'react'
-import NoteContext from './../context/notes/noteContext';
-import { useEffect } from 'react';
 
 export default function Home() {
 
-  const noteVars = useContext(NoteContext);
-  useEffect(() => {
-    noteVars.update('Home');
-  }, [])
-
   return (
     <div className='Home'>
-      <div>This is {noteVars.state.name}</div>
+      <div>This is Home</div>
     </div>
   )
 }
