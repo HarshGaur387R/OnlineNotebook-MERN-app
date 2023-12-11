ac9191467fad991b6e59f284e46e37803270a6eb
Harsh Gaur <hgaur701@gmail.com>
2023-12-11 18:49:06 +0530

    fixed filterChip bug

--- src/components/FilterChip.js
+++ src/components/FilterChip.js
@@ -1,17 +1,16 @@
-import React, { useState } from 'react'
+import React from 'react'
 import chipContext from '../context/filter chips/chipContext'
 import { useContext } from 'react'
 
 export default function FilterChip(prop) {
 
   const {removeChip} = useContext(chipContext);
-  const [chipValue] = useState(prop.value);
 
   return (
     <>
       <div className="badge bg-secondary overflow-hidden d-flex justify-content-around align-items-center">
         <div className="badge-content">
-          {chipValue}
+          {prop.value}
         </div>
         <button onClick={()=>{removeChip(prop.value)}} className="chip-close-btn">&times;</button>
       </div>
